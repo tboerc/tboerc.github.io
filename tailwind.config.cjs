869871d6781d13d5a13153a33cb768acb8c55b50
Tiago Boer Casagrande <tiago.boer@hotmail.com>
2023-08-14 16:19:20 -0300

    feat: font and layout improvements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ['Poppins', defaultTheme.fontFamily.sans],
-				display: ['Oswald Variable', defaultTheme.fontFamily.sans]
+				display: ['Oswald', defaultTheme.fontFamily.sans]
 			}
 		}
 	},
